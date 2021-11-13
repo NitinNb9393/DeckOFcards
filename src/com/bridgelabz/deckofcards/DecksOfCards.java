aaa3665c0af5fee5573260a876ae4705c8c1febe
NitinNb9393 <nitinbabar9393@gmail.com>
2021-11-13 19:50:57 +0530

    Added:To the player

--- src/com/bridgelabz/deckofcards/DecksOfCards.java
+++ src/com/bridgelabz/deckofcards/DecksOfCards.java
@@ -5,8 +5,8 @@ public class DecksOfCards {
 	public static void main(String[] args) {
 		System.out.println("Welcome into the Decks of Cards");
 		CardsPlay cardsPlay = new CardsPlay();
-        cardsPlay.generateUniqueCards();
+		cardsPlay.generateUniqueCards();
+		cardsPlay.addPlayer();
 
 	}
-
 }
