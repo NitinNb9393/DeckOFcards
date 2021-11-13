6535f5b12ecffcbb4b31b30c2e3143a3ae4e9545
NitinNb9393 <nitinbabar9393@gmail.com>
2021-11-13 19:17:00 +0530

    Added:Generate the unique cards

---
+++ src/com/bridgelabz/deckofcards/CardsPlay.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.deckofcards;
+
+public class CardsPlay {
+	String[] cards = new String[52];
+
+	public void generateUniqueCards() {
+
+		String[] suit = { "Clubs", "Diamonds", "Hearts", "Spades" };
+		String[] rank = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
+		int position = 0;
+
+		for (int i = 0; i < suit.length; i++) {
+			for (int j = 0; j < rank.length; j++) {
+
+				cards[position] = suit[i] + rank[j];
+				position++;
+				System.out.println(cards[position] = suit[i] + rank[j]);
+			}
+		}
+	}
+
+}
---
+++ src/com/bridgelabz/deckofcards/DecksOfCards.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.deckofcards;
+
+public class DecksOfCards {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome into the Decks of Cards");
+		CardsPlay cardsPlay = new CardsPlay();
+        cardsPlay.generateUniqueCards();
+
+	}
+
+}
